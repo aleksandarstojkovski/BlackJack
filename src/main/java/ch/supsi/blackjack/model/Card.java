32e21c0f3955115a32af18e16bf4029d5f12f875
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-03-13 17:36:25 +0100

    equals() and hash() of Card

--- src/main/java/ch/supsi/blackjack/model/Card.java
+++ src/main/java/ch/supsi/blackjack/model/Card.java
@@ -1,13 +1,17 @@
 package ch.supsi.blackjack.model;
 
+import java.util.Objects;
+
 public class Card {
 
     private final Seed seed;
     private final Value value;
+
     Card(Seed seed, Value value){
         this.seed = seed;
         this.value = value;
     }
+
     public Seed getSeed() {
         return seed;
     }
@@ -24,5 +28,18 @@ public class Card {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return seed == card.seed &&
+                value == card.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seed, value);
+    }
 
 }
