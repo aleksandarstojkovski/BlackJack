5b5fb68588232cd592c4501fc12b48aea6d0451b
Gatzu78 <attilio.baldini@student.supsi.ch>
2020-04-23 20:59:47 +0200

    Nuova macchina a stati. PlayerAI ha un metodo bet e il compute usa diversi approcci di rischio.

--- backend/src/main/java/ch/supsi/blackjack/model/PlayerAI.java
+++ backend/src/main/java/ch/supsi/blackjack/model/PlayerAI.java
@@ -1,11 +1,11 @@
 package ch.supsi.blackjack.model;
 
 public class PlayerAI implements AI {
-    protected int aiType=0;
+    protected int aiType = 0;
     private final Player aiEntity;
 
-    PlayerAI(Player player){
-        this.aiEntity=player;
+    PlayerAI(Player player) {
+        this.aiEntity = player;
     }
 
     public void setAiType(int aiType) {
@@ -14,6 +14,23 @@ public class PlayerAI implements AI {
 
     @Override
     public void compute(Model model) {
+        if (aiType == 1) {
+            while (aiEntity.getHandValue() < 16) {
+                model.hitInternal(aiEntity);
+            }
+        } else if (aiType == 2){
+            while (aiEntity.getHandValue() < 18) {
+                model.hitInternal(aiEntity);
+            }
+        } else {
+            while (aiEntity.getHandValue() < 17) {
+                model.hitInternal(aiEntity);
+            }
+        }
+    }
 
+    public void bet(Model model){
+        int currentCoins=aiEntity.getCoins();
     }
 }
+
