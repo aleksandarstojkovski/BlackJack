91823af343be16ffe492716da591c2692a379bfa
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-06-05 17:36:21 +0200

    add CoinTest test

--- frontend/src/test/java/ch/supsi/blackjack/CoinTest.java
+++ frontend/src/test/java/ch/supsi/blackjack/CoinTest.java
@@ -2,6 +2,7 @@ package ch.supsi.blackjack;
 
 import ch.supsi.blackjack.model.exception.InvalidCoinValueException;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
@@ -12,6 +13,7 @@ public class CoinTest extends TestCase {
     private Coin testCoin;
     private final Random rand = new Random();
 
+    @Test
     public void testGetValidCoinValue() {
 
         int randomNumberInRange = rand.nextInt(Coin.Values.length);
@@ -27,8 +29,14 @@ public class CoinTest extends TestCase {
 
     }
 
+    @Test
     public void testGetInvalidCoinValue() {
         assertThrows(InvalidCoinValueException.class, ()->new Coin(-1));
+    }
+
+    @Test
+    public void testCoinsAmnt(){
+        assertEquals(Coin.Values.length, 5);
     }
 
 }
