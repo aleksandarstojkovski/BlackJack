f6579faa804b506918e697b7d9fa8d8b0a3c7c36
code-mds <43860962+code-mds@users.noreply.github.com>
2020-06-06 16:43:41 +0200

    AbstractViewTest cover exceptions tests

--- frontend/src/test/java/ch/supsi/blackjack/view/AbstractViewTest.java
+++ frontend/src/test/java/ch/supsi/blackjack/view/AbstractViewTest.java
@@ -1,14 +1,13 @@
 package ch.supsi.blackjack.view;
 
 import ch.supsi.blackjack.CommandCatalog;
-import ch.supsi.blackjack.UITest;
+import ch.supsi.blackjack.controller.AbstractController;
 import ch.supsi.blackjack.controller.ContentAreaController;
 import ch.supsi.blackjack.controller.LogController;
 import ch.supsi.blackjack.controller.MenuController;
 import ch.supsi.blackjack.model.GameHandler;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.experimental.categories.Category;
 import org.mockito.Mockito;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -16,11 +15,37 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ResourceBundle;
 
-@Category(UITest.class)
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//@Category(UITest.class)
 public class AbstractViewTest extends ApplicationTest {
 
     private ResourceBundle bundle;
     private CommandCatalog commandCatalog;
+
+    static class FakeController extends AbstractController {
+        public FakeController(CommandCatalog commandCatalog) {
+            super(commandCatalog);
+        }
+    }
+
+    static class FakeView extends AbstractView {
+        private final static String FXML = "invalid.fxml";
+        private final FakeController controller;
+
+        // Constructor is not public, use Static Factory Method
+        FakeView(FakeController controller) {
+            this.controller = controller;
+        }
+
+        // Static Factory Method
+        public static FakeView create(FakeController controller, ResourceBundle bundle)
+                throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
+            return AbstractView.create(FakeView.class, FXML, controller, bundle);
+        }
+
+    }
 
     @Before
     @SuppressWarnings("SpellCheckingInspection")
@@ -31,38 +56,50 @@ public class AbstractViewTest extends ApplicationTest {
     }
 
     @org.junit.Test
+    public void createViewNullController() {
+        assertThrows(InstantiationException.class, () -> FakeView.create(null, bundle));
+    }
+
+    @org.junit.Test
+    public void createViewNullFXML() {
+        assertThrows(InstantiationException.class, () -> FakeView.create(new FakeController(commandCatalog), bundle));
+    }
+
+    @org.junit.Test
     public void createContentAreaView() {
-        step("create ContentAreaView", () -> {
+//        step("create ContentAreaView", () -> {
             AbstractView view = null;
             try {
                 ContentAreaController controller = new ContentAreaController(commandCatalog);
                 view = ContentAreaView.create(controller, bundle);
             } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException | IOException e) {
                 e.printStackTrace();
+                fail();
             }
 
             Assert.assertNotNull(view);
-        });
+//        });
     }
 
     @org.junit.Test
     public void createLogView() {
-        step("create LogView", () -> {
+//        step("create LogView", () -> {
             AbstractView view = null;
             try {
                 LogController controller = new LogController(commandCatalog);
                 view = LogView.create(controller, bundle);
             } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException | IOException e) {
                 e.printStackTrace();
+                fail();
             }
 
             Assert.assertNotNull(view);
-        });
+//        });
     }
 
     @org.junit.Test
     public void createMenuView() {
-        step("create MenuView", () -> {
+//        step("create MenuView", () -> {
             AbstractView view = null;
             try {
                 MenuController controller = new MenuController(commandCatalog);
@@ -72,11 +109,11 @@ public class AbstractViewTest extends ApplicationTest {
             }
 
             Assert.assertNotNull(view);
-        });
+//        });
     }
 
-    private void step(final String step, final Runnable runnable) {
-        runnable.run();
-    }
+//    private void step(final String step, final Runnable runnable) {
+//        runnable.run();
+//    }
 
 }
